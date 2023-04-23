968edbefab384bda12fca0efa0f90829e0ba9e18
abu al ameen <abulyaqs@gmail.com>
2023-04-23 19:02:23 +0100

    function that produces output according to a format.

--- 0-printf.c
+++ 0-printf.c
@@ -1,82 +1,50 @@
 #include "main.h"
 #include <stdarg.h>
-#include <stdio.h>
 /**
- * _printf - main entry of the function printf
- * @format: string containing the format
+ * _printf - this function prints according to format
+ * @format: given format
  * Return: 0
  */
+
 int _printf(const char *format, ...)
 {
 	va_list args;
 	char c;
 	char *s;
-	int val, t, q;
-	int r = 0;
 	int counter = 0;
-	int arr[MAX];
 
 	va_start(args, format);
-	while (*format)
+	while (*format != '\0')
 	{
 		if (*format == '%')
 		{
 			format++;
-			switch (*format)
+			if (*format == c)
 			{
-				case 'c':
-					c = va_arg(args, int);
-					_putc(c);
+				c = va_arg(args, int);
+				_putc(c);
+				counter++;
+			}
+			else if (*format == '%')
+			{
+				_putc('%');
+				counter++;
+			}
+			else if (*format == 's')
+			{
+				s = va_arg(args, char *);
+				while (*s)
+					_putc(*s);
 					counter++;
-					break;
-				case 's':
-					for (s = va_arg(args, char *); *s; s++)
-					{
-						_putc(*s);
-						counter++;
-					}
-					break;
-				case '%':
-					_putc('%');
-					counter++;
-					break;
-				case 'd':
-				case 'i':
-					val = va_arg(args, int);
-					if (val < 0)
-					{
-						_putc('-');
-						counter++;
-						val = -val;
-					}
-					while (val != 0)
-					{
-						t = val % 10;
-						arr[r] = t;
-						r++;
-						val = val / 10;
-					}
-					for (q = r - 1; q > -1; q--)
-					{
-						_putc(arr[q] + '0');
-					}
-					break;
-				default:
-					_putc('%');
-					_putc(*format);
-					counter += 2;
-					break;
+					s++;
 			}
 		}
 		else
- 		{
 			_putc(*format);
 			counter++;
-		}
-	format++;
+			format++;
+
 	}
 	va_end(args);
 	return (counter);
 }
-
-
