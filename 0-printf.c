0c6d25013f36d8f631972c38acca010692368751
abu al ameen <abulyaqs@gmail.com>
2023-04-23 08:36:21 +0100

    function that produces output according to a format.

--- 0-printf.c
+++ 0-printf.c
@@ -11,7 +11,7 @@ int _printf(const char *format, ...)
 	va_list args;
 	char c;
 	char *s;
-	int val;
+	int val, dgt, temp, base, w;
 	int counter = 0;
 
 	va_start(args, format);
@@ -41,8 +41,39 @@ int _printf(const char *format, ...)
 				case 'd':
 				case 'i':
 					val = va_arg(args, int);
-					printf("%d", val);
-					counter++;
+					if (val < 0)
+					{
+						putchar('-');
+						counter++;
+						val = -val;
+					}
+					dgt = 0;
+					temp = val;
+					while (temp != 0)
+					{
+						dgt++;
+						temp /= 10;
+					}
+					if (dgt == 0)
+					{
+						counter++;
+					}
+					else
+					{
+						base = 1;
+						for (w = 0;  w < dgt; w++)
+						{
+							base *= 10;
+						}
+						while (base != 0)
+						{
+							dgt = val / base;
+							putchar('0' + dgt);
+							counter++;
+							val %= base;
+							base /= 10;
+						}
+					}
 					break;
 				default:
 					putchar('%');
