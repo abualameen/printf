be458dd5bd806fcff31334a11d479d35c488a6ea
abu al ameen <abulyaqs@gmail.com>
2023-04-26 12:32:20 +0100

    function that produces output according to a format.

--- 0-printf.c
+++ 0-printf.c
@@ -1,8 +1,6 @@
 #include "main.h"
 int _putc(char c);
 int str_len(const char *f);
-void int_base_str(int num, int base, char str[]);
-void _int_hand(const char *format, va_list args);
 
 /**
  * _printf - main function
@@ -17,7 +15,7 @@ int _printf(const char *format, ...)
 	char *str;
 
 	va_start(args, format);
-	_int_hand(format, args);
+	/*_int_hand(format, args);*/
 	if (format == NULL)
 		return (-1);
 	while (*format)
@@ -90,89 +88,4 @@ int str_len(const char *f)
 		count++;
 	}
 	return (count);
-}
-
-/**
- * _int_hand - entry of the function printf
- * @format: string containing the format
- * @args: args list
- * Return: 0
- */
-
-void _int_hand(const char *format, va_list args)
-{
-	int val, q;
-	int counter = 0;
-	char st[100];
-
-	while (*format)
-	{
-		if (*format == '%')
-		{
-			format++;
-			if (*format == 'd')
-			{
-				val = va_arg(args, int);
-				int_base_str(val, 10, st);
-				for (q = 0; st[q]; q++)
-				{
-					_putc(st[q]);
-					counter++;
-				}
-			}
-			else if (*format == 'i')
-			{
-				val = va_arg(args, int);
-				int_base_str(val, 10, st);
-				for (q = 0; st[q]; q++)
-				{
-					_putc(st[q]);
-					counter++;
-				}
-			}
-		}
-		else
-		{
-			_putc(*format);
-			counter++;
-		}
-		format++;
-	}
-	va_end(args);
-}
-/**
- * int_base_str - converts int to str
- * @num: num to be converted
- * @base: base of num
- * @str: buffer container
- * Return: 0
- */
-void int_base_str(int num, int base, char str[])
-{
-	int i = 0;
-	int j, sign;
-	int digit;
-	char temp;
-
-	sign = num;
-	if ((sign) < 0)
-		num = -num;
-
-	do {
-		digit = num % base;
-		if (digit < 10)
-			str[i++] = digit + '0';
-		else
-			str[i++] = digit - 10 + 'a';
-		} while ((num /= base) > 0);
-		if (sign < 0)
-			str[i++] = '-';
-
-		str[i] = '\0';
-		for (j = 0; j < i / 2; j++)
-	{
-			temp = str[j];
-			str[j] = str[i - j - 1];
-			str[i - j - 1] = temp;
-	}
 }
