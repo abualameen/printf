6b6b344c2893c54973fb24ccdc94685962b37075
abu al ameen <abulyaqs@gmail.com>
2023-04-27 18:53:09 +0100

    function that produces output according to a format

---
+++ main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	_printf("%b\n", 98);
+	_printf("%b\n", 1024);
+	_printf("%b\n", -1024);
+	_printf("%b\n", 0);
+	return (0);
+}
--- unsignedint_bin_hand.c
+++ unsignedint_bin_hand.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdint.h>
 /**
  * unsignedint_bin_hand - handles unsigned in to binary
  * @format_ptr: format str
@@ -8,18 +9,43 @@
 
 int unsignedint_bin_hand(const char **format_ptr, va_list args)
 {
-	int val, q;
+	uintmax_t val;
+	int q;
 	int counter = 0;
 	char st[100];
 	const char *format;
 
 	format = *format_ptr;
-	val = va_arg(args, int);
-	unint_base_str(val, 2, st);
-	for (q = 0; st[q]; q++)
+	switch (format[-1])
 	{
-		_putc(st[q]);
-		counter++;
+		case 'b':
+			val = va_arg(args, uint32_t);
+			unint_base_str(val, 2, st);
+			for (q = 0; st[q]; q++)
+			{
+				_putc(st[q]);
+				counter++;
+			}
+			break;
+		case 'l':
+		case 'z':
+			val = va_arg(args, uint64_t);
+			unint_base_str(val, 2, st);
+			for (q = 0; st[q]; q++)
+			{
+				_putc(st[q]);
+				counter++;
+			}
+			break;
+		default:
+			val = va_arg(args, unsigned int);
+			unint_base_str(val, 2, st);
+			for (q = 0; st[q]; q++)
+			{
+				_putc(st[q]);
+				counter++;
+			}
+			break;
 	}
 	format++;
 	return (counter);
