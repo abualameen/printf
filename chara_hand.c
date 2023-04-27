63e45725ecc853651788de5cd32a1f48e03dea6a
abu al ameen <abulyaqs@gmail.com>
2023-04-27 10:07:21 +0100

    function that produces output according to a format

--- chara_hand.c
+++ chara_hand.c
@@ -12,10 +12,6 @@ int chara_hand(const char **format_ptr, va_list args)
 	char c;
 	int counter = 0;
 
-	if (*format == '\0')
-	{
-		return (0);
-	}
 	c = va_arg(args, int);
 	_putc(c);
 	counter++;
