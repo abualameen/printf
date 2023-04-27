7f86d1be0db45197c29b1b96db8f6a76f8e1d14e
abu al ameen <abulyaqs@gmail.com>
2023-04-27 13:34:19 +0100

    function that produces output according to a format

--- format_handles.c
+++ format_handles.c
@@ -24,6 +24,8 @@ int format_hand(const char **format_ptr, va_list args)
 		case '%':
 			counter = perc_hand();
 			break;
+		case '\0':
+			return (-1);
 		default:
 			counter += _putc('%');
 			counter += _putc(*format);
